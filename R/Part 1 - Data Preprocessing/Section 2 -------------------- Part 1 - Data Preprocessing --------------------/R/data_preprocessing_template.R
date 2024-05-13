838eedcbc21f2d1e6b1b373a81d8faebbd9a6d9c
Hugo Machefer <hugo.machefer@difuze.com>
2024-05-12 20:01:49 -0400

    36---splitting-the-dataset

--- R/Part 1 - Data Preprocessing/Section 2 -------------------- Part 1 - Data Preprocessing --------------------/R/data_preprocessing_template.R
+++ R/Part 1 - Data Preprocessing/Section 2 -------------------- Part 1 - Data Preprocessing --------------------/R/data_preprocessing_template.R
@@ -22,3 +22,11 @@ dataset$Country = factor(dataset$Country,
 dataset$Purchased = factor(dataset$Purchased,
                          levels = c('No', 'Yes'),
                          labels = c(0, 1))
+
+# splitting the dataset into the Training set and Test set
+# install.packages('caTools')
+library(caTools)
+set.seed(123)
+split = sample.split(dataset$Purchased, SplitRatio = 0.8)
+training_set = subset(dataset, split == TRUE)
+test_set = subset(dataset, split == FALSE)
